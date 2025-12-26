8bf5e4bdea1e08ddba1bc2fcca8f88e3b52cc22a
vanjmali <contact@vanjmali.com>
2025-12-26 23:43:53 +0100

    Add custom spans

--- user-service/services/user_service.go
+++ user-service/services/user_service.go
@@ -16,6 +16,8 @@ import (
 	"github.com/vanjmali/spotlite/user-service/repositories"
 	"github.com/vanjmali/spotlite/user-service/utils/auth"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -44,11 +46,14 @@ var (
 type UserService struct {
 	r  *repositories.UserRepository
 	ms *MailService
+
+	tr trace.Tracer
 }
 
 // NewUserService builds a UserService with repository and mail dependencies.
 func NewUserService(r repositories.UserRepository, ms MailService) *UserService {
-	s := UserService{r: &r, ms: &ms}
+	tr := otel.Tracer("user-service/user-service")
+	s := UserService{r: &r, ms: &ms, tr: tr}
 
 	return &s
 }
@@ -56,84 +61,128 @@ func NewUserService(r repositories.UserRepository, ms MailService) *UserService 
 // Register func, handles registration business logic such as username, email existence validation,
 // sending verification mails.
 func (s *UserService) Register(ctx context.Context, reqDto *dtos.UserRegistrationDto) error {
+	ctx, span := s.tr.Start(ctx, "user.register")
+	defer span.End()
+
+	lookupCtx, lookupSpan := s.tr.Start(ctx, "user.register.lookup_unique")
 	// checks if the username is already taken,
-	exists, err := s.r.ExistsByUsername(ctx, reqDto.Username)
+	exists, err := s.r.ExistsByUsername(lookupCtx, reqDto.Username)
 	if err != nil {
+		lookupSpan.RecordError(err)
+		lookupSpan.End()
 		log.Printf("Error checking if username exists: %v", err)
 		return err
 	}
 	if exists {
+		lookupSpan.End()
 		log.Printf("Username already taken: %s", reqDto.Username)
 		return ErrUsernameTaken
 	}
 
 	// checks if the email is already taken,
-	exists, err = s.r.ExistsByEmail(ctx, reqDto.Email)
+	exists, err = s.r.ExistsByEmail(lookupCtx, reqDto.Email)
 	if err != nil {
+		lookupSpan.RecordError(err)
+		lookupSpan.End()
 		log.Printf("Error checking if email exists: %v", err)
 		return err
 	}
 	if exists {
+		lookupSpan.End()
 		log.Printf("Email already taken: %s", reqDto.Email)
 		return ErrEmailTaken
 	}
+	lookupSpan.End()
 
+	createCtx, createSpan := s.tr.Start(ctx, "user.register.create_user")
 	// if both the username and email are unique we convert the dto into the user entity,
 	// the mapper method does all the heavy lifting and sets the default field values and
 	// hashes the password,
 	userEntity, err := mappers.ToUserEntity(reqDto)
 	if err != nil {
+		createSpan.RecordError(err)
+		createSpan.End()
 		log.Printf("Error converting to user entity: %v", err)
 		return err
 	}
 
+	_, mailSpan := s.tr.Start(ctx, "user.register.send_verification_email")
 	// sends account verification email BEFORE saving to database
 	// if email fails, we don't save the user
 	if err := s.ms.sendAccountVerificationEmail(reqDto.Email, userEntity.EmailVerification.Token); err != nil {
+		mailSpan.RecordError(err)
+		mailSpan.End()
 		log.Printf("Failed to send verification email: %v", err)
 		return err
 	}
+	mailSpan.End()
 
 	// insert the user in the database,
-	err = s.r.Create(ctx, *userEntity)
+	err = s.r.Create(createCtx, *userEntity)
 	if err != nil {
+		createSpan.RecordError(err)
+		createSpan.End()
 		log.Printf("Error creating user in database: %v", err)
 		return err
 	}
+	createSpan.End()
 
 	return nil
 }
 
 // VerifyAccount func, handles account verification business logic.
 func (s *UserService) VerifyAccount(ctx context.Context, token string) error {
-	return s.r.ActiveAndRevokeToken(ctx, token)
+	ctx, span := s.tr.Start(ctx, "user.verify_account")
+	defer span.End()
+
+	if err := s.r.ActiveAndRevokeToken(ctx, token); err != nil {
+		span.RecordError(err)
+		return err
+	}
+	return nil
 }
 
 // Login validates credentials, checks account status, and issues a login OTP.
 func (s *UserService) Login(ctx context.Context, loginDto *dtos.UserLoginDto) error {
-	user, err := s.r.FindUserByEmail(ctx, loginDto.Email)
+	ctx, span := s.tr.Start(ctx, "user.login")
+	defer span.End()
+
+	lookupCtx, lookupSpan := s.tr.Start(ctx, "user.login.lookup_user")
+	user, err := s.r.FindUserByEmail(lookupCtx, loginDto.Email)
 	if err != nil {
+		lookupSpan.RecordError(err)
+		lookupSpan.End()
 		return err
 	}
+	lookupSpan.End()
 
+	_, verifySpan := s.tr.Start(ctx, "user.login.verify_credentials")
 	if user.AccountStatus == account.StatusInactive {
+		verifySpan.End()
 		return ErrUserInactive
 	}
 
 	if time.Now().After(user.PasswordExpiresAt) {
+		verifySpan.End()
 		return ErrExpiredPassword
 	}
 
 	err = auth.CompareHashAndPassword(user.Password, loginDto.Password)
 	if err != nil {
+		verifySpan.RecordError(err)
+		verifySpan.End()
 		return ErrBadCredentials
 	}
+	verifySpan.End()
 
 	return s.sendLoginOtpToUser(ctx, user)
 }
 
 // CreateNewToken issues a signed JWT for the authenticated user.
 func (s *UserService) CreateNewToken(ctx context.Context, user *entities.User) (string, error) {
+	ctx, span := s.tr.Start(ctx, "user.create_token")
+	defer span.End()
+
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
 		"sub":      user.ID,
 		"name":     strings.Join([]string{user.FirstName, user.LastName}, " "),
@@ -146,6 +195,7 @@ func (s *UserService) CreateNewToken(ctx context.Context, user *entities.User) (
 
 	pk, err := utils.GetPrivateKey()
 	if err != nil {
+		span.RecordError(err)
 		return "", err
 	}
 
@@ -156,36 +206,56 @@ func (s *UserService) CreateNewToken(ctx context.Context, user *entities.User) (
 
 // VerifyLoginOtp compares the provided OTP with the stored hash and clears it on success.
 func (s *UserService) VerifyLoginOtp(ctx context.Context, dto *dtos.VerifyLoginOtpDto) (*entities.User, error) {
-	user, err := s.r.FindUserByEmail(ctx, dto.Email)
+	ctx, span := s.tr.Start(ctx, "user.verify_login_otp")
+	defer span.End()
+
+	lookupCtx, lookupSpan := s.tr.Start(ctx, "user.verify_login_otp.lookup_user")
+	user, err := s.r.FindUserByEmail(lookupCtx, dto.Email)
 	if err != nil {
+		lookupSpan.RecordError(err)
+		lookupSpan.End()
 		return nil, err
 	}
+	lookupSpan.End()
 
+	validateCtx, validateSpan := s.tr.Start(ctx, "user.verify_login_otp.validate_code")
 	if user.AccountStatus == account.StatusInactive {
+		validateSpan.End()
 		return nil, ErrUserInactive
 	}
 
 	if user.OTPCode.Hash == "" {
+		validateSpan.End()
 		return nil, ErrOtpInvalid
 	}
 
 	if time.Now().After(user.OTPCode.Expiry) {
-		_ = s.r.ClearLoginOtp(ctx, user.ID)
+		_ = s.r.ClearLoginOtp(validateCtx, user.ID)
+		validateSpan.End()
 		return nil, ErrOtpExpired
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.OTPCode.Hash), []byte(dto.Code)); err != nil {
+		validateSpan.RecordError(err)
+		validateSpan.End()
 		return nil, ErrOtpInvalid
 	}
+	validateSpan.End()
 
-	_ = s.r.ClearLoginOtp(ctx, user.ID)
+	clearCtx, clearSpan := s.tr.Start(ctx, "user.verify_login_otp.clear_otp")
+	_ = s.r.ClearLoginOtp(clearCtx, user.ID)
+	clearSpan.End()
 	return user, nil
 }
 
 // ResendLoginOtp resends the OTP code to the user's email for login verification.
 func (s *UserService) ResendLoginOtp(ctx context.Context, email string) error {
+	ctx, span := s.tr.Start(ctx, "user.resend_login_otp")
+	defer span.End()
+
 	user, err := s.r.FindUserByEmail(ctx, email)
 	if err != nil {
+		span.RecordError(err)
 		return ErrUserNotFound
 	}
 
@@ -198,8 +268,12 @@ func (s *UserService) ResendLoginOtp(ctx context.Context, email string) error {
 
 // sendLoginOtpToUser is a private helper that generates and sends OTP to a user.
 func (s *UserService) sendLoginOtpToUser(ctx context.Context, user *entities.User) error {
+	ctx, span := s.tr.Start(ctx, "user.login.issue_otp")
+	defer span.End()
+
 	otp, err := auth.GenerateOTP()
 	if err != nil {
+		span.RecordError(err)
 		return err
 	}
 
@@ -207,9 +281,11 @@ func (s *UserService) sendLoginOtpToUser(ctx context.Context, user *entities.Use
 
 	// TODO: make time NOT be hardcoded
 	if err := s.r.SetLoginOtp(ctx, user.ID, string(otpHash), time.Now().Add(5*time.Minute)); err != nil {
+		span.RecordError(err)
 		return err
 	}
 	if err := s.ms.SendLoginOtp(user.Email, otp); err != nil {
+		span.RecordError(err)
 		return err
 	}
 
@@ -217,10 +293,24 @@ func (s *UserService) sendLoginOtpToUser(ctx context.Context, user *entities.Use
 }
 
 func (s *UserService) FindByID(ctx context.Context, id primitive.ObjectID) (*entities.User, error) {
-	return s.r.FindUserByID(ctx, id)
+	ctx, span := s.tr.Start(ctx, "user.find_by_id")
+	defer span.End()
+
+	user, err := s.r.FindUserByID(ctx, id)
+	if err != nil {
+		span.RecordError(err)
+	}
+	return user, err
 }
 
 // EmailExists checks if an email is already registered
 func (s *UserService) EmailExists(ctx context.Context, email string) (bool, error) {
-	return s.r.ExistsByEmail(ctx, email)
+	ctx, span := s.tr.Start(ctx, "user.email_exists")
+	defer span.End()
+
+	exists, err := s.r.ExistsByEmail(ctx, email)
+	if err != nil {
+		span.RecordError(err)
+	}
+	return exists, err
 }
