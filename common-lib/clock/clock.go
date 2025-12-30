f8f7df60d4a1a57e8d2745c69816a8ae764280c2
vanjmali <contact@vanjmali.com>
2025-12-30 20:50:34 +0100

    Add comments for clock.go

--- common-lib/clock/clock.go
+++ common-lib/clock/clock.go
@@ -3,6 +3,7 @@ package clock
 import "time"
 
 // Clock provides current time.
+// This allows for easier testing by injecting a mock clock.
 type Clock interface {
 	Now() time.Time
 }
@@ -10,16 +11,20 @@ type Clock interface {
 // RealClock uses the system time.
 type RealClock struct{}
 
+// Now returns the current system time.
 func (RealClock) Now() time.Time { return time.Now() }
 
+// FixedClock always returns a fixed time.
 type FixedClock struct {
 	fixed time.Time
 }
 
+// NewFixedClock creates a FixedClock set to the specified initial time.
 func NewFixedClock(initialTime time.Time) *FixedClock {
 	return &FixedClock{fixed: initialTime}
 }
 
+// Now returns the fixed time.
 func (fc *FixedClock) Now() time.Time {
 	return fc.fixed
 }
