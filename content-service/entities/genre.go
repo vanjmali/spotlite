e6f53bf78c05db40091114416c34c583ead6b77c
stevanovicmilan <stevanovicm1008@gmail.com>
2026-01-15 18:08:57 +0100

    fix remove unused entitie

--- content-service/entities/genre.go
+++
@@ -1,9 +0,0 @@
-package entities
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-// Genre models a genre document stored in MongoDB.
-type Genre struct {
-	ID   primitive.ObjectID `bson:"_id,omitempty"`
-	Name string             `bson:"name,omitempty"`
-}
