7ac9d88fe4d4695ef279d842e87dc74cc81c5e45
vanjmali <contact@vanjmali.com>
2026-01-16 20:27:03 +0100

    Add missing validations to DTOs

--- content-service/dtos/song_dto.go
+++ content-service/dtos/song_dto.go
@@ -7,10 +7,10 @@ import (
 
 // SongDto represents the data transfer object for a song entity.
 type SongDto struct {
-	Title         string   `json:"title" validate:"required"`
-	Genre         string   `json:"genre" validate:"required"`
-	LengthSeconds int      `json:"length_seconds" validate:"required,min=1"`
-	ArtistIds     []string `json:"artist_ids" validate:"required"`
+	Title         string   `json:"title" validate:"required,min=2"`
+	Genre         string   `json:"genre" validate:"required,min=2"`
+	LengthSeconds int      `json:"length_seconds" validate:"required,gt=0"`
+	ArtistIds     []string `json:"artist_ids" validate:"required,min=1,dive,required,len=24,hexadecimal"`
 }
 
 // SongListResponseDto represents the response payload when returning a paginated list of songs.
