0946dc16c99c0fedd3294b04d26b60c2c1274ad7
vukedd <vukedd@gmail.com>
2026-02-03 22:58:19 +0100

    added missing validaiton messages

--- common-lib/requests/validate.go
+++ common-lib/requests/validate.go
@@ -78,6 +78,10 @@ func getErrorMsg(fe validator.FieldError) string {
 		return fe.Field() + " is too short"
 	case "email":
 		return "Invalid email format"
+	case "validentityid":
+		return "Entity ID must be valid."
+	case "validsubtype":
+		return "Subscription type must be valid."
 	default:
 		if msgFunc, exists := customValidationMessages[fe.Tag()]; exists {
 			return msgFunc(fe)
