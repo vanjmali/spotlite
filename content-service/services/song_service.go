e0d850ee557e40344deee2e33d3efd02c1267da2
stevanovicm5 <stevanovicm1008@gmail.com>
2026-02-12 18:21:01 +0100

    fix

--- content-service/services/song_service.go
+++ content-service/services/song_service.go
@@ -138,9 +138,8 @@ func (s *SongService) Create(ctx context.Context, songDto *dtos.SongDto) (primit
 	}
 	mapSpan.End()
 
-	id := songEntity.ID
 	createCtx, createSpan := s.tr.Start(ctx, "song.create.create_song")
-	_, err = s.songRepo.Create(createCtx, *songEntity)
+	id, err := s.songRepo.Create(createCtx, *songEntity)
 	if err != nil {
 		createSpan.RecordError(err)
 		createSpan.End()
