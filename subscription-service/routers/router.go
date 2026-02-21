3978fff79feedc1656ab663699f8ef9fd4675197
Vukasin <137070008+vukedd@users.noreply.github.com>
2026-02-21 23:04:01 +0100

    Genre/ Artist subscriber count (#94)
    Adds entity subscriber count endpoint, so we can display genre/artist
    subscriber count on the page, isn't necessary but seems like a cool
    feature.
    
    Currently the API call doesn't make a distinction when an entity doesn't
    exist or no one is subscribed to it, if get enough time, a check by
    calling the content service via gRPC will be implemented.

--- subscription-service/routers/router.go
+++ subscription-service/routers/router.go
@@ -18,6 +18,7 @@ func HandleRequests(sh *handlers.SubscriptionHandler) http.Handler {
 
 	api.Handle("/{entityID}", middlewares.RequireAuthenticated(sh.HandleIsSubscribed)).Methods("GET")
 	api.Handle("/", middlewares.RequireAuthenticated(sh.HandleSubscribe)).Methods("POST")
+	api.Handle("/count/{entityID}", middlewares.RequireAuthenticated(sh.HandleSubscriberCount)).Methods("GET")
 	api.Handle("/{entityID}", middlewares.RequireAuthenticated(sh.HandleUnsubscribe)).Methods("DELETE")
 	api.Handle("/", middlewares.RequireAuthenticated(sh.HandleUserSubscriptionsList)).Methods("GET")
 
