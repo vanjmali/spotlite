9d98b00972cfb2bcbe3ea6318b4124fd78beb803
vanjmali <contact@vanjmali.com>
2026-03-02 15:03:59 +0100

    Move hardcoded values as env variables

--- user-service/main.go
+++ user-service/main.go
@@ -37,6 +37,7 @@ var (
 	userRatingUpdatedDurable       = "USER_RATING_UPDATED_PROCESSOR"
 	userSubscriptionCreatedDurable = "USER_SUBSCRIPTION_CREATED_PROCESSOR"
 	userSubscriptionDeletedDurable = "USER_SUBSCRIPTION_DELETED_PROCESSOR"
+	userAsynqSchedule              = utils.MustGetEnv("USER_ASYNQ_SCHEDULE")
 	rootCACertFilePath             = utils.MustGetEnv("ROOT_CERT_PATH")
 	certFilePath                   = utils.MustGetEnv("CERT_PATH")
 	keyFilePath                    = utils.MustGetEnv("KEY_PATH")
@@ -287,7 +288,7 @@ func setupAsynq(us *services.UserService, ms *services.MailService) func() error
 
 	// Initialize a scheduler
 	//    minutes *    hours *    day of month *     month *    day of week *
-	as.RegisterSchedule("01 19 * * *")
+	as.RegisterSchedule(userAsynqSchedule)
 
 	// Starts task router and scheduler in separate go routines
 	as.Start(mux)
